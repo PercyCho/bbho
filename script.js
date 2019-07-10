e40abf66678a1fb1a4caa22c6dec33f38f89be57
PercyCho <42121899+PercyCho@users.noreply.github.com>
2019-07-09 20:15:57 -0400

    fixing dark mode

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ if (lastChar = "#") {
 */
 //make sure to fix...all of that
 
-var mode = localStorage.getItem('mode')
+let mode;
+mode = localStorage.getItem('mode');
 
 if (mode = 'light'){
 	lightMode();
@@ -70,7 +71,8 @@ function darkMode() {
 	document.getElementById("darkmodebtn").style.backgroundColor = "white";
 	document.getElementById("darkmodebtn").onclick = lightMode;
 	document.getElementById("darkmodebtn").innerHTML = "Light Mode";
-	localStorage.setItem('mode', 'dark')
+	localStorage.setItem('mode', 'dark');
+	mode = localStorage.getItem('mode');
 }
 
 function lightMode() {
@@ -85,5 +87,6 @@ function lightMode() {
 	document.getElementById("darkmodebtn").style.backgroundColor = "black";
 	document.getElementById("darkmodebtn").onclick = darkMode;
 	document.getElementById("darkmodebtn").innerHTML = "Dark Mode";
-	localStorage.setItem('mode', 'light')
+	localStorage.setItem('mode', 'light');
+	mode = localStorage.getItem('mode');
 }
