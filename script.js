a28d4db170bf504b6b234e2f5423f522c20cbea8
PercyCho <42121899+PercyCho@users.noreply.github.com>
2019-07-18 23:44:47 -0400

    v1.1 - Last major update for a while

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//sleep function if I ever need it
 function sleep(milliseconds) {
 	var start = new Date().getTime();
 	for (var i = 0; i < 1e7; i++) {
@@ -7,11 +8,12 @@ function sleep(milliseconds) {
 	}
 }
 
-var version = "v1.0";
+var version = "v1.1"; //to more easily update each page
 var year = 2019;
 document.getElementById("version").innerHTML = version;
 document.getElementById("year").innerHTML = year + " so far";
 
+//will probs delete commented code below
 /*
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -25,8 +27,8 @@ setTimeout(function deviceCheck() {
 	}
 }, 20);
 */
-//ehhh maybe not
 
+//this code is for the little pop up if you enter without the hashtag in the url.
 function updateTop() {
 	document.getElementById("updating").style.display = "none";
 }
@@ -52,10 +54,13 @@ if (lastChar === "#") {
 	enterSite(0);
 }
 
-if ((urlTest.search("omniverse")>-1) || (urlTest.search("universe")>-1) || (urlTest.search("void")>-1) || (urlTest.search("power")>-1) || (urlTest.search("absolute")>-1)){
+//to go to the 535 error page
+if ((urlTest.search("omniverse")>-1) || (urlTest.search("universe")>-1) || (urlTest.search("void")>-1) || (urlTest.search("power")>-1) || (urlTest.search("absolute")>-1) || (urlTest.search("fsp")>-1)){
 	window.location.replace("https://percycho.github.io/bbho/535.html");
 }
 
+//the light and dark mode! This is fairly easy.
+//local storage is *like* cookies, except it only applies to that site.
 let mode;
 mode = localStorage.getItem('mode');
 
@@ -105,4 +110,50 @@ function lightMode() {
 	document.getElementById("darkmodebtn").innerHTML = "Dark Mode";
 	localStorage.setItem('mode', 'light');
 	mode = localStorage.getItem('mode');
+}
+
+//don't mind beneath here lol
+
+window.addEventListener("keydown", game535, false);
+
+var pos535 = document.getElementById("game535");
+var ver535 = pos535.getBoundingClientRect().top; 
+var hor535 = pos535.getBoundingClientRect().left;
+
+function game535(event) {
+	if (event.keyCode == "38") {
+		ver535-=6;
+		document.getElementById("game535").style.top = ver535 + "px";
+	}
+	if (event.keyCode == "40") {
+		ver535+=6;
+		document.getElementById("game535").style.top = ver535 + "px";
+	}
+	if (event.keyCode == "37") {
+		hor535-=6;
+		document.getElementById("game535").style.left = hor535 + "px";
+	}
+	if (event.keyCode == "39") {
+		hor535+=6;
+		document.getElementById("game535").style.left = hor535 + "px";
+	}
+}
+
+var game535id = document.getElementById('game535');
+var game535off = game535id.getBoundingClientRect();
+var menuBBHOid = document.getElementById('menuBBHO');
+var menuBBHOoff = menuBBHOid.getBoundingClientRect();
+
+cross();
+
+function cross(){
+	game535id = document.getElementById('game535');
+	game535off = game535id.getBoundingClientRect();
+	if ((menuBBHOoff.top < game535off.top) && (menuBBHOoff.bottom > game535off.bottom) && (menuBBHOoff.left < game535off.left) && (menuBBHOoff.right > game535off.right)){
+		document.getElementById('menuFSP').style.display = "inline";
+	}
+	else if ((menuBBHOoff.top > game535off.top) || (menuBBHOoff.bottom < game535off.bottom) || (menuBBHOoff.left > game535off.left) || (menuBBHOoff.right < game535off.right)){
+		document.getElementById('menuFSP').style.display = "none";
+	}
+	setTimeout(cross,100);
 }
